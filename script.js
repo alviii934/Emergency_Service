2dba75a66cc50047aa5efcf4445ca02905716e94
Alvi <hasan2305101696@diu.edu.bd>
2026-01-20 23:10:06 +0600

    Copy Count funtionality added successfully

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
-
+// heart count functoionality
 count = 0;
 document.querySelectorAll('.heart-btn').forEach(heart => {
   heart.addEventListener('click', function (e) {
     e.preventDefault();
     count++;
-    console.log(count);
 
     const availableHeart = parseInt(
       document.getElementById('heart-counts').innerText,
     );
-    console.log(availableHeart);
     const newavailableHeart = availableHeart + count;
     document.getElementById('heart-counts').innerText = newavailableHeart;
     count = 0;
   });
 });
+
+// copy count functoionality
+count1 = 0;
+document.querySelectorAll('.copy-btn').forEach(copy => {
+  copy.addEventListener('click', function (e) {
+    e.preventDefault();
+    count1++;
+    const availableCopy = parseInt(
+      document.getElementById('copy-counter').innerText,
+    );
+    const newavailableCopy = availableCopy + count1;
+    document.getElementById('copy-counter').innerText = newavailableCopy;
+    count1 = 0;
+  });
+});
+
+// call Functionality
