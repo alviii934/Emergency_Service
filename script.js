b9a797e2e58baeb53f05667489b6bd73e9a752e3
Alvi <hasan2305101696@diu.edu.bd>
2026-01-21 12:26:23 +0600

    Calling feature added for all card with out history functionality

--- script.js
+++ script.js
@@ -29,4 +29,138 @@ document.querySelectorAll('.copy-btn').forEach(copy => {
   });
 });
 
-// call Functionality
+// call Functionality for Emergency
+
+document.querySelectorAll('.call-btn').forEach(call => {
+  call.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    console.log('Calling');
+    const coinCounter = document.getElementById('coin-counter');
+    const availableCoin = Number(coinCounter.innerText);
+
+    const emergencyTitle = document.getElementById('emergency-title').innerText;
+
+    const emergencyNumber =
+      document.getElementById('emergency-number').innerText;
+
+    console.log(emergencyTitle, emergencyNumber);
+
+    if (availableCoin >= 20) {
+      alert(`Calling ${emergencyTitle} ${emergencyNumber}`);
+      coinCounter.innerText = availableCoin - 20;
+    } else {
+      alert(' Not enough coins');
+    }
+  });
+});
+
+// call Functionality for Police
+document
+  .getElementById('call-btn-police')
+  .addEventListener('click', function (e) {
+    e.preventDefault();
+
+    console.log('Calling');
+    const coinCounter = document.getElementById('coin-counter');
+    const availableCoin = Number(coinCounter.innerText);
+
+    const policeTitle = document.getElementById('police-title').innerText;
+
+    const policeNumber = document.getElementById('police-number').innerText;
+
+    console.log(policeTitle, policeNumber, availableCoin);
+
+    if (availableCoin >= 20) {
+      alert(`Calling ${policeTitle} ${policeNumber}`);
+      coinCounter.innerText = availableCoin - 20;
+    } else {
+      alert(' Not enough coins ');
+    }
+  });
+
+
+// call Functionality for Fire Service
+  document
+    .getElementById('call-btn-fire')
+    .addEventListener('click', function (e) {
+      e.preventDefault();
+
+      const coinCounter = document.getElementById('coin-counter');
+      const availableCoin = Number(coinCounter.innerText);
+
+      const fireTitle = document.getElementById('fire-title').innerText;
+      const fireNumber = document.getElementById('fire-number').innerText;
+
+      if (availableCoin >= 20) {
+        alert(`Calling ${fireTitle} ${fireNumber}`);
+        coinCounter.innerText = availableCoin - 20;
+      } else {
+        alert('Not enough coins');
+      }
+    });
+
+
+
+// call Functionality for Ambulance Service
+document
+  .getElementById('call-btn-ambulance')
+  .addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const coinCounter = document.getElementById('coin-counter');
+    const availableCoin = Number(coinCounter.innerText);
+
+    const ambulanceTitle = document.getElementById('ambulance-title').innerText;
+    const ambulanceNumber = document.getElementById('ambulance-number').innerText;
+
+    if (availableCoin >= 20) {
+      alert(`Calling ${ambulanceTitle} ${ambulanceNumber}`);
+      coinCounter.innerText = availableCoin - 20;
+    } else {
+      alert('Not enough coins');
+    }
+  });
+
+
+// call Functionality for Woman Child Helpline
+document
+  .getElementById('call-btn-woman')
+  .addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const coinCounter = document.getElementById('coin-counter');
+    const availableCoin = Number(coinCounter.innerText);
+
+    const womanTitle = document.getElementById('woman-title').innerText;
+    const womanNumber = document.getElementById('woman-number').innerText;
+
+    if (availableCoin >= 20) {
+      alert(`Calling ${womanTitle} ${womanNumber}`);
+      coinCounter.innerText = availableCoin - 20;
+    } else {
+      alert('Not enough coins');
+    }
+  });
+
+
+
+// call Functionality for Anti Corruption Helpline
+document
+  .getElementById('call-btn-anti')
+  .addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const coinCounter = document.getElementById('coin-counter');
+    const availableCoin = Number(coinCounter.innerText);
+
+    const antiTitle = document.getElementById('anti-title').innerText;
+    const antiNumber = document.getElementById('anti-number').innerText;
+
+    if (availableCoin >= 20) {
+      alert(`Calling ${antiTitle}: ${antiNumber}`);
+      coinCounter.innerText = availableCoin - 20;
+    } else {
+      alert('Not enough coins');
+    }
+  });
